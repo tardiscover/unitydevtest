ad742f4f2eb628c58553ca3f164d308102cda347
tardiscover <87391515+tardiscover@users.noreply.github.com>
2023-09-14 00:05:21 -0400

    Link AttributeSliderController to mesh

--- Assets/Scripts/AttributeSliderController.cs
+++ Assets/Scripts/AttributeSliderController.cs
@@ -16,6 +16,24 @@ public class AttributeSliderController : MonoBehaviour
 
     public void OnSliderValueChanged()
     {
-        Debug.Log($"OnSliderValueChanged {sliderComponent.value}");
+        Debug.Log($"OnSliderValueChanged {sliderComponent.value}"); //!!!
+
+        int blendShapeIndex;
+
+        foreach (BlendShapeSource blendShapeSource in blendShapeSources)
+        {
+            //Debug.Log($"OnSliderValueChanged - blendShapeCount = {blendShapeSource.skinnedMeshRenderer.sharedMesh.blendShapeCount}"); //!!!
+
+            blendShapeIndex = blendShapeSource.skinnedMeshRenderer.sharedMesh.GetBlendShapeIndex(blendShapeSource.blendShape);  //!!!
+
+            if (blendShapeIndex >= 0)
+            {
+                blendShapeSource.skinnedMeshRenderer.SetBlendShapeWeight(blendShapeIndex, sliderComponent.value);
+            }
+            else
+            {
+                Debug.Log($"OnSliderValueChanged - No blendShapeIndex for {blendShapeSource.skinnedMeshRenderer.name}, {blendShapeSource.blendShape}"); //!!!
+            }
+        }
     }
 }
